daa4932d4e24bccec143071473c83e258a33b224
Tibor Molnár <fatalaa@hotmail.com>
2014-07-01 22:34:20 +0200

    fixed chart re-rendering bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,8 @@ function fetchLatest() {
                 response = json;
                 appendDynamicElements(json);
                 hideLoadingIndicator();
+                $('#chart_container').hide();
+                $('#load_chart_button').removeAttr('disabled');
             });
         },
         error: function(xhr, status) {
@@ -26,6 +28,10 @@ function fetchHistory() {
         type: 'GET',
         dataType: 'json',
         success: function(json) {
+            $('#chart_container').remove();
+            var chartContainer = getChartDOM();
+            $('.container').append(chartContainer);
+            
             var palette = new Rickshaw.Color.Palette({scheme: 'munin'});
 
             var graph = new Rickshaw.Graph({
@@ -106,4 +112,41 @@ function getSeriesForGraph(data, palette) {
         });
     }
     return series;
+}
+
+function getChartDOM() {
+    var chartContainer = $('<div></div>');
+    chartContainer.addClass('hidden');
+    chartContainer.attr('id', 'chart_container');
+    
+    var chartDiv = $('<div></div>');
+    chartDiv.addClass('rickshaw_graph');
+    chartDiv.attr('id', 'chart');
+    
+    var detailDiv = $('<div></div>');
+    detailDiv.addClass('detail');
+    chartDiv.append(detailDiv);
+    
+    var legendContainer = $('<div></div>');
+    legendContainer.attr('id', 'legend_container');
+    
+    var smootherDiv = $('<div></div>');
+    smootherDiv.attr('id', 'smoother');
+    smootherDiv.attr('title', 'Smoothing');
+    
+    var legendDiv = $('<div></div>');
+    legendDiv.attr('id', 'legend');
+    legendDiv.addClass('rickshaw_legend');
+    
+    legendContainer.append(smootherDiv);
+    legendContainer.append(legendDiv);
+    
+    var sliderDiv = $('<div></div>');
+    sliderDiv.attr('id', 'slider');
+    
+    chartContainer.append(chartDiv);
+    chartContainer.append(legendContainer);
+    chartContainer.append(sliderDiv);
+    
+    return chartContainer;
 }
